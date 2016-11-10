d053b2b0a0057734a9321c4ac48c2e0f186d9802
Safikur Khan <SafikurRahaman.Khan@walgreens.com>
2016-11-10 01:19:21 -0600

    modified

--- myRetail/src/test/java/com/test/myretail/products/service/TestProductService.java
+++ myRetail/src/test/java/com/test/myretail/products/service/TestProductService.java
@@ -8,6 +8,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import com.myretail.products.service.ProductService;
 import com.myretail.products.views.Price;
+import com.myretail.products.views.Product;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration({"classpath:test-myretail-config.xml"})
@@ -16,15 +17,25 @@ public class TestProductService {
 	@Autowired
 	private ProductService productService;
 	
-	@Test
+//	@Test
 	public void getProductPrice() throws Exception {
 		Price price = productService.getProductPrice(15117729);
 		System.out.println("currency:"+price.getCurrencyCode());
 		System.out.println("price:"+price.getValue());
 	}
 	
-//	@Test
-	public void getTargetProduct() throws Exception {
-//		System.out.println(result.getResponse().getContentAsString());
+	@Test
+	public void updateProductPrice() throws Exception {
+		Product product = new Product();
+		product.setId(15117729);
+		product.setName("Test");
+		
+		Price price = new Price();
+		price.setValue(13.44);
+		price.setCurrencyCode("USD");
+		
+		product.setPrice(price);
+		
+		productService.updateProductPrice(product);
 	}
 }
